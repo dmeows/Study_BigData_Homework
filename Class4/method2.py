90c70755f011beb5048a262c9273f99445200a62
Dmeows <habaokhanh239@gmail.com>
2023-10-25 11:31:22 +0700

    refactor

--- Class4/method2.py
+++ Class4/method2.py
@@ -1,87 +1,70 @@
-#%% import libs
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-from pyspark.sql.functions import when
-import pyspark.sql.functions as sf
-import os 
+from pyspark.sql.functions import col, when
+from datetime import datetime
+import os
 
-#%% set up env
-spark = SparkSession.builder.config("spark.driver.memory", "8g").config("spark.executor.cores",8).getOrCreate()
+def etl_process(path, file_name):
+    spark = SparkSession.builder.config("spark.driver.memory", "8g").config("spark.executor.cores", 8).getOrCreate()
 
-#%% Read, map and pivot data in 1 day
-def etl_1_day(path ,file_name):
-    #print('---------------------')
-    #print('Read data from Kafka')
-    #print('---------------------')
-    df = spark.read.json(path+file_name)
-    #print('---------------------')
-    #print('Showing top 5')
-    #print('---------------------')
-    #df.show()
-    #print('---------------------')
-    #print('Showing data structure')
-    #print('---------------------')
-    #df.printSchema()
-    #print('---------------------')
-    #print('Showing data value')
-    #print('---------------------')
+    df = spark.read.json(path + file_name)
     df = df.select('_source.*')
-    # df.show()
-    # print('---------------------')
-    # print('ETL Start')
-    # print('---------------------')
+
     df = df.withColumn("Type",
-           when((col("AppName") == 'CHANNEL') | (col("AppName") =='DSHD')| (col("AppName") =='KPLUS')| (col("AppName") =='KPlus'), "Truyền Hình")
-        .when((col("AppName") == 'VOD') | (col("AppName") =='FIMS_RES')| (col("AppName") =='BHD_RES')| 
-              (col("AppName") =='VOD_RES')| (col("AppName") =='FIMS')| (col("AppName") =='BHD')| (col("AppName") =='DANET'), "Phim Truyện")
-        .when((col("AppName") == 'RELAX'), "Giải Trí")
-        .when((col("AppName") == 'CHILD'), "Thiếu Nhi")
-        .when((col("AppName") == 'SPORT'), "Thể Thao")
-        .otherwise("Error"))
-    # print('---------------------')
-    # print('Showing new data structure')
-    # print('---------------------')
-    df = df.select('Contract','Type','TotalDuration')
+                       when((col("AppName") == 'CHANNEL') | (col("AppName") == 'DSHD') | (col("AppName") == 'KPLUS') | (col("AppName") == 'KPlus'), "Truyền Hình")
+                       .when((col("AppName") == 'VOD') | (col("AppName") == 'FIMS_RES') | (col("AppName") == 'BHD_RES') |
+                             (col("AppName") == 'VOD_RES') | (col("AppName") == 'FIMS') | (col("AppName") == 'BHD') | (
+                                     col("AppName") == 'DANET'), "Phim Truyện")
+                       .when((col("AppName") == 'RELAX'), "Giải Trí")
+                       .when((col("AppName") == 'CHILD'), "Thiếu Nhi")
+                       .when((col("AppName") == 'SPORT'), "Thể Thao")
+                       .otherwise("Error"))
+
+    df = df.select('Contract', 'Type', 'TotalDuration')
     df = df.filter(df.Type != 'Error')
-    # df.printSchema()
-    # df.select('Contract','Type','TotalDuration').show(5)
-    df = df.groupBy('Contract','Type').sum()
-    df = df.withColumnRenamed('sum(TotalDuration)','TotalDuration')
-    # df = df.cache()
-    # print('---------------------')
-    # print('Pivoting Data')
-    # print('---------------------')
+
+    #Process one df at a time
+    result = df.groupBy('Contract', 'Type').sum()
+    result = result.withColumnRenamed('sum(TotalDuration)', 'TotalDuration')
     result = df.groupBy("Contract").pivot("Type").sum("TotalDuration")
-    # print('---------------------')
-    # print('Showing result')
-    # print('---------------------')
-    # result.show(5)
+
     print('Finished Processing {}'.format(file_name))
     return result
 
-#%% Append to the remaining days
-def main_task():
+
+def main_task(start_date_str, end_date_str):
+    start_time = datetime.now()
     path = "/Users/habaokhanh/Study_BigData_Dataset/log_content/"
-    list_file = os.listdir(path)
-    file_name = list_file[0]
-    result1 = etl_1_day(path ,file_name)
-    for i in list_file[1:]:
-        file_name2 = i 
-        result2 = etl_1_day(path ,file_name2)
-        result1 = result1.union(result2)
-        #result1 = result1.cache()
+    list_file = [file for file in os.listdir(path) if file != '.DS_Store']
     
-    #small adjustment to method1
-    result1 = result1.groupby('Contract').sum()
-    result1 = result1.withColumnRenamed('sum(Giải Trí)','RelaxDuration').\
-        withColumnRenamed('sum(Phim Truyện)','MovieDuration').\
-            withColumnRenamed('sum(Thiếu Nhi)','ChildDuration').\
-                withColumnRenamed('sum(Thể Thao)','SportDuration').\
-                    withColumnRenamed('sum(Truyền Hình)','TVDuration')
-    #
+    result = None
+    for file_name in list_file:
+        date_str = file_name.split('_')[-1].split('.')[0]
+        file_date = datetime.strptime(date_str, "%Y%m%d").date()
+        
+        start_date = datetime.strptime(start_date_str, "%Y%m%d").date()
+        end_date = datetime.strptime(end_date_str, "%Y%m%d").date()
+
+        if start_date <= file_date <= end_date:
+            df = etl_process(path, file_name)
+            if result is None:
+                result = df
+            else:
+                result = result.union(df)
+
+    result = result.groupby('Contract').sum()
+    result = result.withColumnRenamed('sum(Giải Trí)', 'RelaxDuration') \
+        .withColumnRenamed('sum(Phim Truyện)', 'MovieDuration') \
+        .withColumnRenamed('sum(Thiếu Nhi)', 'ChildDuration') \
+        .withColumnRenamed('sum(Thể Thao)', 'SportDuration') \
+        .withColumnRenamed('sum(Truyền Hình)', 'TVDuration')
+
     print('-----------Saving Data ---------')
-    result1.repartition(1).write.csv('/Users/habaokhanh/Study_BigData_Dataset/log_content/df_clean2',header=True)
+    result.repartition(1).write.csv('/Users/habaokhanh/Study_BigData_Dataset/log_content/df_clean2', header=True)
+    
+    end_time = datetime.now()
+    time_processing = (end_time - start_time).total_seconds()
+    print('It took {} to process the data'.format(time_processing))
     return print('Data Saved Successfully')
 
-main_task()
 
+main_task('20220401', '20220402')
